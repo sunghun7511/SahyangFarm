f198e7d8261316a396ef3bd114f38614877ee98c
KSHGroup <kshgroup@kshgroup.kr>
2021-08-17 07:15:44 +0900

    feat: 관리자용 팜 증설 명령어 구현

--- src/main/java/kr/kshgroup/sahyangfarm/command/CommandManager.java
+++ src/main/java/kr/kshgroup/sahyangfarm/command/CommandManager.java
@@ -2,6 +2,7 @@ package kr.kshgroup.sahyangfarm.command;
 
 import kr.kshgroup.sahyangfarm.ManagerBase;
 import kr.kshgroup.sahyangfarm.command.admin.CommandForceMove;
+import kr.kshgroup.sahyangfarm.command.admin.CommandMaxUserIncrease;
 import org.bukkit.Bukkit;
 
 public class CommandManager extends ManagerBase {
@@ -15,6 +16,7 @@ public class CommandManager extends ManagerBase {
     public void onRun() {
         mainCommand = new CommandFarm();
         mainCommand.registerCommand(new CommandForceMove());
+        mainCommand.registerCommand(new CommandMaxUserIncrease());
 
         Bukkit.getPluginCommand("팜").setExecutor(mainCommand);
         Bukkit.getPluginCommand("farm").setExecutor(mainCommand);
--- src/main/java/kr/kshgroup/sahyangfarm/command/admin/CommandMaxUserIncrease.java
+++ src/main/java/kr/kshgroup/sahyangfarm/command/admin/CommandMaxUserIncrease.java
@@ -1,4 +1,44 @@
 package kr.kshgroup.sahyangfarm.command.admin;
 
-public class CommandMaxUserIncrease {
+import kr.kshgroup.sahyangfarm.Reference;
+import kr.kshgroup.sahyangfarm.SahyangFarm;
+import kr.kshgroup.sahyangfarm.command.SFCommandExecutor;
+import kr.kshgroup.sahyangfarm.model.Farm;
+import kr.kshgroup.sahyangfarm.story.StoryManager;
+import kr.kshgroup.sahyangfarm.story.farm.StoryFarm;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.util.Objects;
+
+public class CommandMaxUserIncrease extends SFCommandExecutor {
+    private final StoryManager storyManager;
+
+    public CommandMaxUserIncrease() {
+        super("증설", "increase");
+
+        storyManager = (StoryManager) SahyangFarm.getManager(StoryManager.class);
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, String label, String[] args) {
+        if (isNotPlayer(sender)) return true;
+        Player player = (Player) sender;
+
+        if (!player.isOp()) {
+            player.sendMessage(Reference.PREFIX + "이 명령어를 사용할 권한이 없습니다.");
+            return true;
+        }
+
+        StoryFarm storyFarm = storyManager.getStory(StoryFarm.class);
+        Farm farm = storyFarm.getFarm(player);
+        if (Objects.isNull(farm)) {
+            player.sendMessage(Reference.PREFIX + "팜을 생성하지 않았습니다.");
+            player.sendMessage(Reference.PREFIX + "/팜 명령어를 통해 팜을 생성해주세요.");
+            return true;
+        }
+
+        farm.setMaxUser(farm.getMaxUser() + 1);
+        return false;
+    }
 }
