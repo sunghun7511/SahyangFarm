c24f89f4ae32ab5cb1a30ad1a7574622a6b0cf31
KSHGroup <kshgroup@kshgroup.kr>
2021-08-17 04:21:04 +0900

    feat: 팜 월드 이름 Reference 추가

--- src/main/java/kr/kshgroup/sahyangfarm/Reference.java
+++ src/main/java/kr/kshgroup/sahyangfarm/Reference.java
@@ -2,4 +2,5 @@ package kr.kshgroup.sahyangfarm;
 
 public final class Reference {
     public static final String PREFIX = "§a[ §e으내듀밸리 §a] ";
+    public static final String WORLD_NAME = "farm";
 }
