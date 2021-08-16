26298fe92e0fa3b017126e451a7f208d789dcbab
KSHGroup <kshgroup@kshgroup.kr>
2021-08-17 08:40:27 +0900

    chore: 명령어 출력 색상 변경

--- src/main/java/kr/kshgroup/sahyangfarm/Reference.java
+++ src/main/java/kr/kshgroup/sahyangfarm/Reference.java
@@ -1,7 +1,7 @@
 package kr.kshgroup.sahyangfarm;
 
 public final class Reference {
-    public static final String PREFIX = "§a[ §e으내듀밸리 §a] ";
+    public static final String PREFIX = "§a[ §e으내듀밸리 §a] §f";
     public static final String WORLD_NAME = "farm";
 
     public static final String SCHEMATIC_FILENAME = "farm.schematic";
