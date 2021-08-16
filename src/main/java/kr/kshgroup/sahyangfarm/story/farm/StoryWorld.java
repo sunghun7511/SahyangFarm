09f70e4008d087599c63fc2382d37c71732bd1a0
KSHGroup <kshgroup@kshgroup.kr>
2021-08-17 04:21:28 +0900

    feat: 월드 사용에 관한 로직 구현

---
+++ src/main/java/kr/kshgroup/sahyangfarm/story/farm/StoryWorld.java
@@ -0,0 +1,26 @@
+package kr.kshgroup.sahyangfarm.story.farm;
+
+import kr.kshgroup.sahyangfarm.Reference;
+import kr.kshgroup.sahyangfarm.story.SFStoryBase;
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+import org.bukkit.WorldCreator;
+import org.bukkit.WorldType;
+
+import java.util.Optional;
+
+public class StoryWorld implements SFStoryBase {
+    public World getWorld() {
+        return Optional.ofNullable(Bukkit.getWorld(Reference.WORLD_NAME))
+                .orElseGet(this::createNewWorld);
+    }
+
+    private World createNewWorld() {
+        WorldCreator wc = new WorldCreator(Reference.WORLD_NAME);
+
+        wc.type(WorldType.FLAT);
+        wc.generatorSettings("2;0;1;");
+
+        return wc.createWorld();
+    }
+}
