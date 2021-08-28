678cb425d73e463a45afe17b5e944a6e939aebe0
KSHGroup <kshgroup@kshgroup.kr>
2021-08-29 02:10:36 +0900

    feat: OP인 경우 팜 이동이 강제로 가능하도록 변경

--- src/main/java/kr/kshgroup/sahyangfarm/command/farm/CommandFarmMove.java
+++ src/main/java/kr/kshgroup/sahyangfarm/command/farm/CommandFarmMove.java
@@ -46,7 +46,7 @@ public class CommandFarmMove extends SFCommandExecutor {
             return true;
         }
 
-        if (!farm.isAllowMove()) {
+        if (!player.isOp() && !farm.isAllowMove()) {
             player.sendMessage(Reference.PREFIX + "팜 주인이 이동을 원하지 않습니다.");
             return true;
         }
