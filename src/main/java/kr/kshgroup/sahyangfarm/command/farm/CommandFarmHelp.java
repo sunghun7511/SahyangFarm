8332c9509e944a09cc76f5a272c2a90386d7e282
KSHGroup <kshgroup@kshgroup.kr>
2021-08-29 02:30:15 +0900

    feat: 팜 알바 관련 도움말 추가

--- src/main/java/kr/kshgroup/sahyangfarm/command/farm/CommandFarmHelp.java
+++ src/main/java/kr/kshgroup/sahyangfarm/command/farm/CommandFarmHelp.java
@@ -56,6 +56,8 @@ public class CommandFarmHelp extends SFCommandExecutor {
         FARM_PARTY_KICK("/팜 파티 추방 <닉네임>", "내 팜의 파티에서 유저를 추방합니다."),
         FARM_PARTY_LEAVE("/팜 파티 나가기", "소속된 파티에서 나갑니다.", "초대받은 경우만 사용이 가능합니다."),
         FARM_TRANSFER("/팜 파티 양도 <닉네임>", "내 팜의 소유권을 양도합니다."),
+        FARM_PART_TIME_ADD("/팜 알바 고용 <닉네임>", "내 팜의 권한을 임시로 추가합니다.", "승인 과정이 없고, 알바 유저의 팜은 유지됩니다."),
+        FARM_PART_TIME_REMOVE("/팜 알바 해고 <닉네임>", "부여했던 권한을 다시 가져옵니다."),
         FARM_OP_INCREASE("/팜 증설", true, "스크립트 연동용 명령어입니다.", "자신의 팜의 최대 파티 인원을 1명 증설합니다."),
         FARM_OP_SET_MAX_USER("/팜 파티 인원설정 <닉네임> <최대 인원>", true, "특정 유저의 최대 인원을 설정합니다."),
         FARM_OP_MAX_ENTITY("/팜 엔티티제한 (최대 엔티티 수)", true, "서버 전체의 엔티티 제한을 설정합니다.", "값을 입력하지 않으면 현재 값을 확인합니다."),
