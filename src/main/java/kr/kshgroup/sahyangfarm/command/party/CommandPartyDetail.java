c0c9826482ffe6e9830af25c7d3962cf96f53fba
KSHGroup <kshgroup@kshgroup.kr>
2021-08-29 02:29:01 +0900

    feat: 팜 확인 명령어에서 고용된 알바 목록 표시

--- src/main/java/kr/kshgroup/sahyangfarm/command/party/CommandPartyDetail.java
+++ src/main/java/kr/kshgroup/sahyangfarm/command/party/CommandPartyDetail.java
@@ -8,7 +8,6 @@ import kr.kshgroup.sahyangfarm.story.StoryManager;
 import kr.kshgroup.sahyangfarm.story.farm.StoryFarm;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
-import org.bukkit.OfflinePlayer;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
@@ -39,14 +38,23 @@ public class CommandPartyDetail extends SFCommandExecutor {
 
         player.sendMessage(Reference.PREFIX + "=-=-=-=[ 팜 정보 ]=-=-=-=");
         player.sendMessage(Reference.PREFIX + "주인 : " + farm.getOwnerName());
+
         List<UUID> users = farm.getUsers();
         if (users.size() != 0) {
             player.sendMessage(Reference.PREFIX + "소속된 유저 (" + users.size() + " / " + farm.getMaxUser() + "):");
             for (UUID user : users) {
-                OfflinePlayer offlinePlayer = Bukkit.getOfflinePlayer(user);
-                player.sendMessage(Reference.PREFIX + " - " + offlinePlayer.getName());
+                player.sendMessage(Reference.PREFIX + " - " + Bukkit.getOfflinePlayer(user).getName());
             }
         }
+
+        List<UUID> parttimes = farm.getUsers();
+        if (parttimes.size() != 0) {
+            player.sendMessage(Reference.PREFIX + "고용된 알바 (" + parttimes.size() + "명):");
+            for (UUID user : parttimes) {
+                player.sendMessage(Reference.PREFIX + " - " + Bukkit.getOfflinePlayer(user).getName());
+            }
+        }
+
         player.sendMessage(Reference.PREFIX + "상태 : " + (farm.isAllowMove() ? "열림" : "닫힘 (이동 불가)"));
 
         Location center = farm.getCenter();
